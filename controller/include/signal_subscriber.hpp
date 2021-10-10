aef5a548ddd7fea38b4d8348ab6c80b46a5a5eac
suyu-hash <nabenabe11317@ezweb.ne.jp>
2021-10-10 23:03:18 +0900

    add some ...

--- controller/include/signal_subscriber.hpp
+++ controller/include/signal_subscriber.hpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Int8MultiArray.h>
+#include "determining_input_value,hpp"
 
 namespace aqua{
     class Signal_Subscriber{
@@ -8,13 +9,19 @@ namespace aqua{
     public:
         Signal_Subscriber();
         ~Signal_Subscriber();
+        void cal_motor_input_val();
+        void send_msg();
 
     private:
         ros::NodeHandle nh;
-        void signal_callback(const geometry_msgs::Twist &msg);
+        void signal_callback(const geometry_msgs::TwistPtr &msg);
 
     private:
         ros::Subscriber signal_sub;
         ros::Publisher motor_data_pub;
+        std_msgs::Int8MultiArray motor_data;
+        
+        float u[6];
+        float xd[6];
     };
 }
