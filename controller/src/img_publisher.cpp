156f2e06bb420940da451d7eab632727f8101334
tetsu00 <tt3836225@gmail.com>
2021-10-23 22:11:43 +0900

    delete include for OpenCV

--- controller/src/img_publisher.cpp
+++ controller/src/img_publisher.cpp
@@ -1,9 +1,5 @@
 #include<ros/ros.h>
 #include<stdio.h>
-#include<cv_bridge/cv_bridge.h>
-#include<image_transport/image_transport.h>
-#include<opencv2/core.hpp>
-#include<opencv2/highgui.hpp>
 #include "cv_camera/driver.h"
 
 int main(int argc, char **argv){
@@ -11,9 +7,6 @@ int main(int argc, char **argv){
     ros::init(argc,argv,"img_publisher");
     ros::NodeHandle private_node("~");
     cv_camera::Driver driver(private_node, private_node);
-    
-    image_transport::ImageTransport it(nh);
-    image_transport::Publisher img_pub = it.advertise("image",10);
 
     try{
         driver.setup();
