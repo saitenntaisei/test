0086b4fbf4128482c8623f0ccc306d0e66a7f790
tetsu00 <tt3836225@gmail.com>
2021-10-24 14:37:43 +0900

    fix img_subscriber

--- controller/src/img_subscriber.cpp
+++ controller/src/img_subscriber.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv){
     ros::NodeHandle nh;
 
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber image_sub = it.subscribe("/image_publisher/image_raw",1,imageCallback);
+    image_transport::Subscriber image_sub = it.subscribe("/img_publisher/image_raw",1,imageCallback);
     ros::spin();
     return 0;
 }
