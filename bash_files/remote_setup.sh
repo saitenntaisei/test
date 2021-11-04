1fec26a4392ad87f15febb37888ccae96a385664
Yuki Origane <origane.y.aa@m.titech.ac.jp>
2021-11-04 17:46:05 +0900

    modify remote_side config

--- bash_files/remote_setup.sh
+++ bash_files/remote_setup.sh
@@ -2,4 +2,6 @@ export ROS_MASTER_URI=http://192.168.2.100:11311
 export ROS_IP=192.168.2.101
 
 source ~/catkin_kurione_ws/devel/setup.bash
+source ~/cv_bridge_ws/install/setup.bash
+source ~/cv_bridge_ws/devel/setup.bash
 cd ~/catkin_kurione_ws/src/2021_Clione
